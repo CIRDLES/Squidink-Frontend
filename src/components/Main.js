b263064a0f2a641b72bda423d6eaa940b843230b
RichardMcCarty1 <58711057+RichardMcCarty1@users.noreply.github.com>
2021-03-23 22:40:43 -0400

    Dropped Header within SquidInk Content

--- src/components/Main.js
+++ src/components/Main.js
@@ -56,154 +56,6 @@ export class Main extends React.Component {
         const { totalReactPackages, fbrdisplay } = this.state;
         return (
             <div className={cx('container')}>
-                <div className={cx('header', 'panel')}> <div className="rownav navbar">
-                    <div className="dropdown">
-                        <button className="dropbtn">Project
-                            <i className="fa fa-caret-down"></i>
-                        </button>
-                        <div className="dropdown-content">
-                            <a className="DefaultState" href="#">Manage Project</a>
-                            <a className="dropdown-content-child" href="#">New Squid GEOCHRON Project</a>
-                            <a className="dropdown-content-child" href="#">New Squid RATIO Project &lt;BETA&gt; </a>
-                            <a className="dropdown-content-child" href="#">Open Squid Project</a>
-                            <a className="dropdown-content-child" href="#">Open Recent Squid Project</a>
-                            <a className="" href="#">Open Demonstration Squid Project</a>
-                            <a className="DefaultState" href="#">Save Squid Project</a>
-                            <a className="DefaultState" href="#">Save Squid Project as ...</a>
-                            <a className="DefaultState" href="#">Close Squid Project</a>
-                            <a className="DefaultState" href="#">Quit Squid</a>
-                            {this.state.showfbr ?
-                                <a onClick={this.hidediv}className="DefaultState" href="#">Hide FB</a>
-                                : <a onClick={this.hidediv}className="DefaultState" href="#">Show FB</a>}
-                        </div>
-                    </div>
-                    <div className="dropdown">
-                        <button className="dropbtn">Data
-                            <i className="fa fa-caret-down"></i>
-                        </button>
-                        <div className="dropdown-content">
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
-                        </div>
-                    </div>
-                    <div className="dropdown">
-                        <button className="dropbtn">Task--&gt;
-                            <i className="fa fa-caret-down"></i>
-                        </button>
-                        <div className="dropdown-content">
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
-                        </div>
-                    </div>
-                    <div className="dropdown">
-                        <button className="dropbtn">Isotopes
-                            <i className="fa fa-caret-down"></i>
-                        </button>
-                        <div className="dropdown-content">
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
-                        </div>
-                    </div>
-                    <div className="dropdown">
-                        <button className="dropbtn">Expressions
-                            <i className="fa fa-caret-down"></i>
-                        </button>
-                        <div className="dropdown-content">
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
-                        </div>
-                    </div>
-                    <div className="dropdown">
-                        <button className="dropbtn">Common Pb
-                            <i className="fa fa-caret-down"></i>
-                        </button>
-                        <div className="dropdown-content">
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
-                        </div>
-                    </div>
-                    <div className="dropdown">
-                        <button className="dropbtn">Interpretations
-                            <i className="fa fa-caret-down"></i>
-                        </button>
-                        <div className="dropdown-content">
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
-                        </div>
-                    </div>
-                    <div className="dropdown">
-                        <button className="dropbtn">Reports
-                            <i className="fa fa-caret-down"></i>
-                        </button>
-                        <div className="dropdown-content">
-                            <a href="#">Custom Report Builder</a>
-                            <a href="#">Summary Expressions and Values</a>
-                            <a href="#">Project Audit</a>
-                            <a href="#">Task Audit</a>
-                            <a onClick={this.clickActionGen} href="#">Generate All Reports</a>
-                            <a href="#">Misc. Reports</a>
-                        </div>
-                    </div>
-                    <div className="dropdown">
-                        <button className="dropbtn">Archiving
-                            <i className="fa fa-caret-down"></i>
-                        </button>
-                        <div className="dropdown-content">
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
-                        </div>
-                    </div>
-                    <div className="dropdown">
-                        <button className="dropbtn">Parameters
-                            <i className="fa fa-caret-down"></i>
-                        </button>
-                        <div className="dropdown-content">
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
-                        </div>
-                    </div>
-                    <div className="dropdown">
-                        <button className="dropbtn">About
-                            <i className="fa fa-caret-down"></i>
-                        </button>
-                        <div className="dropdown-content">
-                            <a href="#">About Squid3</a>
-                            <a href="#">How to Cite Squid3</a>
-                            <a href="#">Squid3 Github Repository</a>
-                            <a href="#">Squid3 Development Notes</a>
-                            <a href="#">CIRDLES.org</a>
-                            <a href="#">Topsoil Github Repository</a>
-                            <a href="#">Enjoy!</a>
-                        </div>
-                    </div>
-                    <div className="dropdown">
-                        <button className="dropbtn">Help
-                            <i className="fa fa-caret-down"></i>
-                        </button>
-                        <div className="dropdown-content">
-                            <a href="#">Introduction and Guide to Menu</a>
-                            <a href="#">Video Tutorials</a>
-                            <a href="#">Contribute an Issue on Github</a>
-                        </div>
-                    </div>
-                    <div className="dropdown">
-                        <button className="dropbtn">My Files
-                            <i className="fa fa-caret-down"></i>
-                        </button>
-                        <div className="dropdown-content">
-                            <a href="#" onClick="fileManager()">File Manager</a>
-                        </div>
-                    </div>
-                </div>
-                </div>
                 <div className={cx('body')}>
                     {this.state.showfbr ?
                         <ResizePanel  direction="e" style={{ id: 'fbr',flexGrow: '1'}} >
@@ -213,6 +65,154 @@ export class Main extends React.Component {
                         </ResizePanel>
                         : null}
                     <div className={cx('content')} style={{ display: 'flex', overflow: 'hidden'}}>
+                        <div className={cx('header', 'panel')} style={{position: 'absolute', top: 0}}> <div className="rownav navbar">
+                            <div className="dropdown">
+                                <button className="dropbtn">Project
+                                    <i className="fa fa-caret-down"></i>
+                                </button>
+                                <div className="dropdown-content">
+                                    <a className="DefaultState" href="#">Manage Project</a>
+                                    <a className="dropdown-content-child" href="#">New Squid GEOCHRON Project</a>
+                                    <a className="dropdown-content-child" href="#">New Squid RATIO Project &lt;BETA&gt; </a>
+                                    <a className="dropdown-content-child" href="#">Open Squid Project</a>
+                                    <a className="dropdown-content-child" href="#">Open Recent Squid Project</a>
+                                    <a className="" href="#">Open Demonstration Squid Project</a>
+                                    <a className="DefaultState" href="#">Save Squid Project</a>
+                                    <a className="DefaultState" href="#">Save Squid Project as ...</a>
+                                    <a className="DefaultState" href="#">Close Squid Project</a>
+                                    <a className="DefaultState" href="#">Quit Squid</a>
+                                    {this.state.showfbr ?
+                                        <a onClick={this.hidediv}className="DefaultState" href="#">Hide FB</a>
+                                        : <a onClick={this.hidediv}className="DefaultState" href="#">Show FB</a>}
+                                </div>
+                            </div>
+                            <div className="dropdown">
+                                <button className="dropbtn">Data
+                                    <i className="fa fa-caret-down"></i>
+                                </button>
+                                <div className="dropdown-content">
+                                    <a href="#">Link 1</a>
+                                    <a href="#">Link 2</a>
+                                    <a href="#">Link 3</a>
+                                </div>
+                            </div>
+                            <div className="dropdown">
+                                <button className="dropbtn">Task--&gt;
+                                    <i className="fa fa-caret-down"></i>
+                                </button>
+                                <div className="dropdown-content">
+                                    <a href="#">Link 1</a>
+                                    <a href="#">Link 2</a>
+                                    <a href="#">Link 3</a>
+                                </div>
+                            </div>
+                            <div className="dropdown">
+                                <button className="dropbtn">Isotopes
+                                    <i className="fa fa-caret-down"></i>
+                                </button>
+                                <div className="dropdown-content">
+                                    <a href="#">Link 1</a>
+                                    <a href="#">Link 2</a>
+                                    <a href="#">Link 3</a>
+                                </div>
+                            </div>
+                            <div className="dropdown">
+                                <button className="dropbtn">Expressions
+                                    <i className="fa fa-caret-down"></i>
+                                </button>
+                                <div className="dropdown-content">
+                                    <a href="#">Link 1</a>
+                                    <a href="#">Link 2</a>
+                                    <a href="#">Link 3</a>
+                                </div>
+                            </div>
+                            <div className="dropdown">
+                                <button className="dropbtn">Common Pb
+                                    <i className="fa fa-caret-down"></i>
+                                </button>
+                                <div className="dropdown-content">
+                                    <a href="#">Link 1</a>
+                                    <a href="#">Link 2</a>
+                                    <a href="#">Link 3</a>
+                                </div>
+                            </div>
+                            <div className="dropdown">
+                                <button className="dropbtn">Interpretations
+                                    <i className="fa fa-caret-down"></i>
+                                </button>
+                                <div className="dropdown-content">
+                                    <a href="#">Link 1</a>
+                                    <a href="#">Link 2</a>
+                                    <a href="#">Link 3</a>
+                                </div>
+                            </div>
+                            <div className="dropdown">
+                                <button className="dropbtn">Reports
+                                    <i className="fa fa-caret-down"></i>
+                                </button>
+                                <div className="dropdown-content">
+                                    <a href="#">Custom Report Builder</a>
+                                    <a href="#">Summary Expressions and Values</a>
+                                    <a href="#">Project Audit</a>
+                                    <a href="#">Task Audit</a>
+                                    <a onClick={this.clickActionGen} href="#">Generate All Reports</a>
+                                    <a href="#">Misc. Reports</a>
+                                </div>
+                            </div>
+                            <div className="dropdown">
+                                <button className="dropbtn">Archiving
+                                    <i className="fa fa-caret-down"></i>
+                                </button>
+                                <div className="dropdown-content">
+                                    <a href="#">Link 1</a>
+                                    <a href="#">Link 2</a>
+                                    <a href="#">Link 3</a>
+                                </div>
+                            </div>
+                            <div className="dropdown">
+                                <button className="dropbtn">Parameters
+                                    <i className="fa fa-caret-down"></i>
+                                </button>
+                                <div className="dropdown-content">
+                                    <a href="#">Link 1</a>
+                                    <a href="#">Link 2</a>
+                                    <a href="#">Link 3</a>
+                                </div>
+                            </div>
+                            <div className="dropdown">
+                                <button className="dropbtn">About
+                                    <i className="fa fa-caret-down"></i>
+                                </button>
+                                <div className="dropdown-content">
+                                    <a href="#">About Squid3</a>
+                                    <a href="#">How to Cite Squid3</a>
+                                    <a href="#">Squid3 Github Repository</a>
+                                    <a href="#">Squid3 Development Notes</a>
+                                    <a href="#">CIRDLES.org</a>
+                                    <a href="#">Topsoil Github Repository</a>
+                                    <a href="#">Enjoy!</a>
+                                </div>
+                            </div>
+                            <div className="dropdown">
+                                <button className="dropbtn">Help
+                                    <i className="fa fa-caret-down"></i>
+                                </button>
+                                <div className="dropdown-content">
+                                    <a href="#">Introduction and Guide to Menu</a>
+                                    <a href="#">Video Tutorials</a>
+                                    <a href="#">Contribute an Issue on Github</a>
+                                </div>
+                            </div>
+                            <div className="dropdown">
+                                <button className="dropbtn">My Files
+                                    <i className="fa fa-caret-down"></i>
+                                </button>
+                                <div className="dropdown-content">
+                                    <a href="#" onClick="fileManager()">File Manager</a>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
                         <h1>Squid Stuff Here</h1>
                         <p>
                         </p>
